6da3f50345a1e1bcc752ddffbe8f1a7b6baba1c6
luokj <luokeju2011@gmail.com>
2016-05-19 16:37:13 +0800

    remove GPL

--- unicorn/fan.h
+++ unicorn/fan.h
@@ -1,21 +1,6 @@
 /*
  * Unicorn 3D Printer Firmware
  * fan.h
- *
- * Copyright (c) 2014 Truby Zong <truby.zong@gmail.com>
- *
- * Unicorn is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Unicorn is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this package; if not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef _FAN_H
 #define _FAN_H
